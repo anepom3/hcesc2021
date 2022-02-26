420f84efa186d2d2fb062faa15d13bf170997d00
anepom3 <anepom3@illinois.edu>
2022-02-25 22:26:42 -0600

    Added plane challenge.

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    // Variables
+    private GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Offset Camera to the side of the plane
+        transform.position = plane.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Private Variables
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // Get input from keyboard 
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move vehicle forward or back
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Turn vehicle left or right
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
