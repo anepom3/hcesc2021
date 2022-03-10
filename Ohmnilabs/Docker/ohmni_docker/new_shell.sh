ce5c14c90447ce371048143de2503b780ddd73a8
anepom3 <anepom3@illinois.edu>
2022-03-09 21:27:53 -0600

    Modified some of the docker scripts.

--- Ohmnilabs/Docker/ohmni_docker/new_shell.sh
+++ Ohmnilabs/Docker/ohmni_docker/new_shell.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# docker exec -it ohmni_ros bash
 docker exec -it ohmni_ros bash
 
--- Ohmnilabs/Docker/ohmni_docker/setup.sh
+++ Ohmnilabs/Docker/ohmni_docker/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker pull ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.8.1
-docker run -it -d --name ohmni_ros --privileged -p 10000:10000 -p 5005:5005 -v /dev:/dev ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.8.1 bash
+# docker run -it -d --name ohmni_ros --privileged -p 10000:10000 -p 5005:5005 -v /dev:/dev ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.8.1 bash
+docker run -it --name ohmni_ros --privileged -p 10000:10000 -p 5005:5005 -v /dev:/dev ohmnilabsvn/ohmni_ros:ohmni_ros_tbcontrol_0.0.8.1 bash
