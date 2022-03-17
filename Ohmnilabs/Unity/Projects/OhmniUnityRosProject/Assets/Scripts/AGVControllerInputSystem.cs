9534bc0237317d860363ef129dec537c72b97ec5
anepom3 <anepom3@illinois.edu>
2022-03-17 10:08:01 -0500

    Added new features for camera, controller, vitals monitor

---
+++ Ohmnilabs/Unity/Projects/OhmniUnityRosProject/Assets/Scripts/AGVControllerInputSystem.cs
@@ -0,0 +1,184 @@
+#if ENABLE_INPUT_SYSTEM 
+using UnityEngine.InputSystem;
+#endif
+
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Geometry;
+using Unity.Robotics.UrdfImporter.Control;
+
+namespace RosSharp.ControlInputSystem
+{
+    public enum ControlMode { Keyboard, ROS};
+
+    public class AGVControllerInputSystem : MonoBehaviour
+    {
+        public GameObject wheel1;
+        public GameObject wheel2;
+        public ControlMode mode = ControlMode.ROS;
+
+        private ArticulationBody wA1;
+        private ArticulationBody wA2;
+
+        public float maxLinearSpeed = 2; //  m/s
+        public float maxRotationalSpeed = 1;//
+        public float wheelRadius = 0.033f; //meters
+        public float trackWidth = 0.288f; // meters Distance between tyres
+        public float forceLimit = 10;
+        public float damping = 10;
+
+        public float ROSTimeout = 0.5f;
+        private float lastCmdReceived = 0f;
+
+        ROSConnection ros;
+        private RotationDirection direction;
+        private float rosLinear = 0f;
+        private float rosAngular = 0f;
+
+    #if ENABLE_INPUT_SYSTEM
+        InputAction movement;
+    #endif
+
+        void Start()
+        {
+            wA1 = wheel1.GetComponent<ArticulationBody>();
+            wA2 = wheel2.GetComponent<ArticulationBody>();
+            SetParameters(wA1);
+            SetParameters(wA2);
+            ros = ROSConnection.GetOrCreateInstance();
+            ros.Subscribe<TwistMsg>("cmd_vel", ReceiveROSCmd);
+
+            #if ENABLE_INPUT_SYSTEM
+            movement = new InputAction("PlayerMovement", binding: "<Gamepad>/leftStick");
+            movement.AddCompositeBinding("Dpad")
+                .With("Up", "<Keyboard>/w")
+                .With("Down", "<Keyboard>/s")
+                .With("Left", "<Keyboard>/a")
+                .With("Right", "<Keyboard>/d");
+            movement.Enable();
+            #endif
+        }
+
+        void ReceiveROSCmd(TwistMsg cmdVel)
+        {
+            rosLinear = (float)cmdVel.linear.x;
+            rosAngular = (float)cmdVel.angular.z;
+            lastCmdReceived = Time.time;
+        }
+
+        void FixedUpdate()
+        {
+            if (mode == ControlMode.Keyboard)
+            {
+                KeyBoardUpdate();
+            }
+            else if (mode == ControlMode.ROS)
+            {
+                ROSUpdate();
+            }     
+        }
+
+        private void SetParameters(ArticulationBody joint)
+        {
+            ArticulationDrive drive = joint.xDrive;
+            drive.forceLimit = forceLimit;
+            drive.damping = damping;
+            joint.xDrive = drive;
+        }
+
+        private void SetSpeed(ArticulationBody joint, float wheelSpeed = float.NaN)
+        {
+            ArticulationDrive drive = joint.xDrive;
+            if (float.IsNaN(wheelSpeed))
+            {
+                drive.targetVelocity = ((2 * maxLinearSpeed) / wheelRadius) * Mathf.Rad2Deg * (int)direction;
+            }
+            else
+            {
+                drive.targetVelocity = wheelSpeed;
+            }
+            joint.xDrive = drive;
+        }
+
+        private void KeyBoardUpdate()
+        {
+            float moveDirection;
+            float turnDirection;
+            float inputSpeed;
+            float inputRotationSpeed;
+#if ENABLE_INPUT_SYSTEM
+            var delta = movement.ReadValue<Vector2>();
+            moveDirection = delta.y;
+            turnDirection = delta.x;
+#else
+            moveDirection = Input.GetAxis("Vertical");
+            turnDirection = Input.GetAxis("Horizontal");
+#endif
+            if (moveDirection > 0)
+            {
+                inputSpeed = maxLinearSpeed;
+            }
+            else if (moveDirection < 0)
+            {
+                inputSpeed = maxLinearSpeed * -1;
+            }
+            else
+            {
+                inputSpeed = 0;
+            }
+
+            
+            if (turnDirection > 0)
+            {
+                inputRotationSpeed = maxRotationalSpeed;
+            }
+            else if (turnDirection < 0)
+            {
+                inputRotationSpeed = maxRotationalSpeed * -1;
+            }
+            else
+            {
+                inputRotationSpeed = 0;
+            }
+            RobotInput(inputSpeed, inputRotationSpeed);
+        }
+
+
+        private void ROSUpdate()
+        {
+            if (Time.time - lastCmdReceived > ROSTimeout)
+            {
+                rosLinear = 0f;
+                rosAngular = 0f;
+            }
+            RobotInput(rosLinear, -rosAngular);
+        }
+
+        private void RobotInput(float speed, float rotSpeed) // m/s and rad/s
+        {
+            if (speed > maxLinearSpeed)
+            {
+                speed = maxLinearSpeed;
+            }
+            if (rotSpeed > maxRotationalSpeed)
+            {
+                rotSpeed = maxRotationalSpeed;
+            }
+            float wheel1Rotation = (speed / wheelRadius);
+            float wheel2Rotation = wheel1Rotation;
+            float wheelSpeedDiff = ((rotSpeed * trackWidth) / wheelRadius);
+            if (rotSpeed != 0)
+            {
+                wheel1Rotation = (wheel1Rotation + (wheelSpeedDiff / 1)) * Mathf.Rad2Deg;
+                wheel2Rotation = (wheel2Rotation - (wheelSpeedDiff / 1)) * Mathf.Rad2Deg;
+            }
+            else
+            {
+                wheel1Rotation *= Mathf.Rad2Deg;
+                wheel2Rotation *= Mathf.Rad2Deg;
+            }
+            SetSpeed(wA1, wheel1Rotation);
+            SetSpeed(wA2, wheel2Rotation);
+        }
+    }
+}
